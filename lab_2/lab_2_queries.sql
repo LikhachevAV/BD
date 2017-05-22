20a75f4e3fcb27b45e79e4acbd8fb0f5e545a60d
Артем Лихачев <artem.lihachev@travelline.ru>
2017-05-22 18:08:32 +0300

    Добавил запрос задания 1.1

--- lab_2/lab_2_queries.sql
+++ lab_2/lab_2_queries.sql
@@ -1,6 +1,5 @@
 CREATE database uefa_euro_2012;
 USE uefa_euro_2012;
-
 ####CREATE TABLES
 
 CREATE TABLE team (
@@ -47,16 +46,27 @@ INSERT INTO team (team_code, team_name, coach) VALUES
   ("POL", "Poland", "Franciszek Smuda"),
   ("RUS", "Russia", "Dick Advocaat"),
   ("CZE", "Czech Republic", "Michal Bilek"),
-  ("GRE", "Greece", "Fernando Santos");
+  ("GRE", "Greece", "Fernando Santos"),
+  ("GER", "Germany", "Yoakhim Lev"),
+  ("POR", "Portugal", "Paulu Bentu");
   
 INSERT INTO game (match_date, stadium, team1, team2) VALUES
   ("2012-06-08", "National Stadium, Warsaw", "1", "4"),
   ("2012-06-08", "Stadion Miejski, Wroclaw", "2", "3"),
   ("2012-06-12", "Stadion Miejski, Wroclaw", "4", "3"),
-  ("2012-06-12", "National Stadium, Warsaw", "1", "2");
+  ("2012-06-12", "National Stadium, Warsaw", "1", "2"),
+  ("2012-06-10", "Arena Lvov, Lvov", "5", "6");
   
-  INSERT INTO goal (match_id, team_id, player, match_time) VALUES
+INSERT INTO goal (match_id, team_id, player, match_time) VALUES
   ("1", "1", "Robert Lewandowski", "17"),
   ("1", "4", "Dimitris Salpingidis", "51"),
   ("2", "2", "Alan Dzagoev", "15"),
-  ("1", "2", "Roman Pavlyuchenko", "82");
+  ("1", "2", "Roman Pavlyuchenko", "82"),
+  ("5", "5", "Mario Gomes", "72");
+  
+###SELECTS:
+#1
+SELECT game.match_id, goal.player
+ FROM (game LEFT JOIN goal ON game.match_id = goal.match_id)
+   LEFT JOIN team ON goal.team_id = team.team_id
+ WHERE team.team_code="GER";
