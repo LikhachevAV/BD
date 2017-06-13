be18f2dfd737df5bd48013d2dd11faff5220a9cd
Artem <temik_l@mail.ru>
2017-06-14 00:22:44 +0300

    Добавил каркас проекта для редиса

---
+++ lab_5/src/main/java/ru/artem/redistest/Main.java
@@ -0,0 +1,14 @@
+package ru.artem.redistest;
+
+/**
+ * Created by tamerlan on 13.06.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*REDIS settings:
+        * redis-11878.c14.us-east-1-2.ec2.cloud.redislabs.com:11878
+        dbTest
+        rePass#333*/
+        System.out.println("Hello, world!");
+    }
+}
