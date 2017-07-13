ebadfc0c11432e3e0568da00c2ed4c652a812a79
Artem <temik_l@mail.ru>
2017-07-14 02:03:21 +0300

    Добавил файл проекта 4й лабы

---
+++ lab_4/src/main/java/ru/artem/ormexample/Command.java
@@ -0,0 +1,7 @@
+package ru.artem.ormexample;
+
+/**
+ * Created by tamerlan on 13.07.2017.
+ */
+public enum Command {
+}
---
+++ lab_4/src/main/java/ru/artem/ormexample/Main.java
@@ -0,0 +1,21 @@
+package ru.artem.ormexample;
+
+import java.io.IOException;
+
+/**
+ * Created by tamerlan on 13.06.2017.
+ */
+public class Main {
+
+    /*
+    Добавить нового участника
+    Удалить участника
+    Добавить голос
+    Отобрать голос
+    Получить список всех участников, отсортированных по количеству голосов
+    */
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
